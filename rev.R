7d053e7c9fd011a0a97dc2bac5fca6dbdd88c016
tnaake <thomasnaake@gmx.de>
2014-11-09 19:02:15 +0100

    function to create a reverse of a given character

--- rev.R
+++ rev.R
@@ -1,14 +1,14 @@
 
 
 reverse <- function(x) {
-    x <- strsplit(x, split = NULL)
-    x <- x[[1]]
-    y <- x
-    y[which(x == "A")] <- "T"
-    y[which(x == "T")] <- "A"
-    y[which(x == "G")] <- "C"
-    y[which(x == "C")] <- "G"
-    y[length(y):1] <- y[1:length(y)]
-    y <- paste0(y, collapse="")
-    return(y)
+    .x <- strsplit(x, split = NULL)
+    .x <- .x[[1]]
+    .y <- .x
+    .y[which(.x == "A")] <- "T"
+    .y[which(.x == "T")] <- "A"
+    .y[which(.x == "G")] <- "C"
+    .y[which(.x == "C")] <- "G"
+    .y[length(.y):1] <- .y[1:length(.y)]
+    ans <- paste0(.y, collapse="")
+    return(ans)
 }
