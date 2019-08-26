290b5e7a5c676ff3603ee237be9c679103b1bd11
tnaake <thomasnaake@gmx.de>
2019-08-26 19:39:14 +0200

    add workflow for other samples than ML

--- ms2_similarity.R
+++ ms2_similarity.R
@@ -260,30 +260,68 @@ ML_cid40 <- ML_cid40[ML_cid40[, "MSMS.spectrum"] != "", cols_keep]
 ML_hcd30 <- ML_hcd30[ML_hcd30[, "MSMS.spectrum"] != "", cols_keep]
 ML_hcd40 <- ML_hcd40[ML_hcd40[, "MSMS.spectrum"] != "", cols_keep]
 ML_hcd50 <- ML_hcd50[ML_hcd50[, "MSMS.spectrum"] != "", cols_keep]
+swM_cid30 <- swM_cid30[swM_cid30[, "MSMS.spectrum"] != "", cols_keep]
+swM_cid40 <- swM_cid40[swM_cid40[, "MSMS.spectrum"] != "", cols_keep]
+swM_hcd30 <- swM_hcd30[swM_hcd30[, "MSMS.spectrum"] != "", cols_keep]
+swM_hcd40 <- swM_hcd40[swM_hcd40[, "MSMS.spectrum"] != "", cols_keep]
+swM_hcd50 <- swM_hcd50[swM_hcd50[, "MSMS.spectrum"] != "", cols_keep]
 
 ## 4) 
 ## truncate files that they only contain the spectra that are aligned
 ## (spectra that are present in aln)
+## kernel CID
 MK_cid30 <- MK_cid30[MK_cid30[, "PeakID"] %in% aln[, "mixMK.ddms2.CID30"],]
 MK_cid40 <- MK_cid40[MK_cid40[, "PeakID"] %in% aln[, "mixMK.ddms2.CID40"],]
-
+## kernel HCD
 MK_hcd30 <- MK_hcd30[MK_hcd30[, "PeakID"] %in% aln[, "mixMK.ddm2.HCD30"],]
 MK_hcd40 <- MK_hcd40[MK_hcd40[, "PeakID"] %in% aln[, "mixMK.ddms2.HCD40"],]
 MK_hcd50 <- MK_hcd50[MK_hcd50[, "PeakID"] %in% aln[, "mixMK.ddms2.HCD50"],]
 
+## leaf CID
 ML_cid30 <- ML_cid30[ML_cid30[, "PeakID"] %in% aln[, "mixML.ddms2.CID30"],]
 ML_cid40 <- ML_cid40[ML_cid40[, "PeakID"] %in% aln[, "mixML.ddms2.CID40"],]
-
+## leaf HCD
 ML_hcd30 <- ML_hcd30[ML_hcd30[, "PeakID"] %in% aln[, "mixML.ddm2.HCD30"],]
 ML_hcd40 <- ML_hcd40[ML_hcd40[, "PeakID"] %in% aln[, "mixML.ddms2.HCD40"],]
 ML_hcd50 <- ML_hcd50[ML_hcd50[, "PeakID"] %in% aln[, "mixML.ddms2.HCD50"],]
 
+## sweet maize
+swM_cid30 <- swM_cid30[swM_cid30[, "PeakID"] %in% aln[, "QC.ddms.CID30"],]
+swM_cid40 <- swM_cid40[swM_cid40[, "PeakID"] %in% aln[, "QC.ddms.CID40"],]
+swM_hcd30 <- swM_hcd30[swM_hcd30[, "PeakID"] %in% aln[, "QC.ddms.HCD30"],]
+swM_hcd40 <- swM_hcd40[swM_hcd40[, "PeakID"] %in% aln[, "QC.ddms.HCD40"],]
+swM_hcd50 <- swM_hcd50[swM_hcd50[, "PeakID"] %in% aln[, "QC.ddms.HCD50"],]
+
+
 
 ## 5) 
 ## assemble spectra
+assembly_MK_cid <- assemblySpectra(spectra=list(MK_cid30, MK_cid40), 
+    aln=aln, cols_aln=c("mixMK.ddm2.CID30", "mixMK.ddms2.CID40"),
+    sample_name="MK_CID")
 assembly_MK_hcd <- assemblySpectra(spectra=list(MK_hcd30, MK_hcd40, MK_hcd50), 
     aln=aln, cols_aln=c("mixMK.ddm2.HCD30", "mixMK.ddms2.HCD40", "mixMK.ddms2.HCD50"),
     sample_name="MK_HCD")
+assembly_ML_cid <- assemblySpectra(spectra=list(ML_cid30, ML_cid40), 
+    aln=aln, cols_aln=c("mixML.ddm2.CID30", "mixML.ddms2.CID40"),
+    sample_name="ML_CID")
+assembly_ML_hcd <- assemblySpectra(spectra=list(ML_hcd30, ML_hcd40, ML_hcd50), 
+    aln=aln, cols_aln=c("mixML.ddm2.HCD30", "mixML.ddms2.HCD40", "mixML.ddms2.HCD50"),
+    sample_name="ML_HCD")
+assembly_swM_cid <- assemblySpectra(spectra=list(swM_cid30, swM_cid40), 
+    aln=aln, cols_aln=c("QC.ddm2.CID30", "QC.ddms2.CID40"),
+    sample_name="swM_CID")
+assembly_swM_hcd <- assemblySpectra(spectra=list(swM_hcd30, swM_hcd40, swM_hcd50), 
+    aln=aln, cols_aln=c("QC.ddms.HCD30", "QC.ddms.HCD40", "QC.ddms.HCD50"),
+    sample_name="swM_HCD")
+
+save(assembly_MK_cid, file="ms2_assembly_MK_cid.RData")
+save(assembly_MK_hcd, file="ms2_assembly_MK_hcd.RData")
+save(assembly_ML_cid, file="ms2_assembly_ML_cid.RData")
+save(assembly_ML_hcd, file="ms2_assembly_ML_hcd.RData")
+save(assembly_swM_cid, file="ms2_assembly_swM_cid.RData")
+save(assembly_swM_hcd, file="ms2_assembly_swM_hcd.RData")
+
 
 ## 6) 
 ## convert final deconvoluted spectra to Spectrum2 objects from MSnbase
@@ -291,30 +329,108 @@ assembly_MK_hcd <- assemblySpectra(spectra=list(MK_hcd30, MK_hcd40, MK_hcd50),
 ## load the MSnbase package
 library(MSnbase)
 
-## retrieve retention time, precursorMZ, m/z of fragments and corresponding 
+## retrieve retention time, precursorMZ, m/z of peaks and corresponding 
 ## intensities from the assembled spectrum
+## kernel CID
+rt_MK_cid <- unlist(lapply(strsplit(names(assembly_MK_cid), split="_"), "[", 4))
+rt_MK_cid <- as.numeric(rt_MK_cid)
+prec_mz_MK_cid <- unlist(lapply(strsplit(names(assembly_MK_cid), split="_"), "[", 5))
+prec_mz_MK_cid <- as.numeric(prec_mz_MK_cid)
+mz_MK_cid <- lapply(assembly_MK_cid, function(x) x[,1])
+int_MK_cid <- lapply(assembly_MK_cid, function(x) x[,2])
+## create Spectrum2 objects with the corresponding information
+spN_MK_cid_l <- lapply(1:length(assembly_MK_cid), function(x) new("Spectrum2", 
+                                                                  rt=rt_MK_cid[x], precursorMz=prec_mz_MK_cid[x], 
+                                                                  mz=mz_MK_cid[[x]], intensity=int_MK_cid[[x]]))
+## kernel HCD
 rt_MK_hcd <- unlist(lapply(strsplit(names(assembly_MK_hcd), split="_"), "[", 4))
 rt_MK_hcd <- as.numeric(rt_MK_hcd)
 prec_mz_MK_hcd <- unlist(lapply(strsplit(names(assembly_MK_hcd), split="_"), "[", 5))
 prec_mz_MK_hcd <- as.numeric(prec_mz_MK_hcd)
 mz_MK_hcd <- lapply(assembly_MK_hcd, function(x) x[,1])
 int_MK_hcd <- lapply(assembly_MK_hcd, function(x) x[,2])
-
 ## create Spectrum2 objects with the corresponding information
-spN_l <- lapply(1:length(assembly_MK_hcd), function(x) new("Spectrum2", 
-            rt=rt_MK_hcd[x], precursorMz=prec_mz_MK_hcd[x], 
-            mz=mz_MK_hcd[[x]], intensity=int_MK_hcd[[x]]))
+spN_MK_hcd_l <- lapply(1:length(assembly_MK_hcd), function(x) new("Spectrum2", 
+                                                                  rt=rt_MK_hcd[x], precursorMz=prec_mz_MK_hcd[x], 
+                                                                  mz=mz_MK_hcd[[x]], intensity=int_MK_hcd[[x]]))
+
+## leaf CID
+rt_ML_cid <- unlist(lapply(strsplit(names(assembly_ML_cid), split="_"), "[", 4))
+rt_ML_cid <- as.numeric(rt_ML_cid)
+prec_mz_ML_cid <- unlist(lapply(strsplit(names(assembly_ML_cid), split="_"), "[", 5))
+prec_mz_ML_cid <- as.numeric(prec_mz_ML_cid)
+mz_ML_cid <- lapply(assembly_ML_cid, function(x) x[,1])
+int_ML_cid <- lapply(assembly_ML_cid, function(x) x[,2])
+## create Spectrum2 objects with the corresponding information
+spN_ML_cid_l <- lapply(1:length(assembly_ML_cid), function(x) new("Spectrum2", 
+                                                                  rt=rt_ML_cid[x], precursorMz=prec_mz_ML_cid[x], 
+                                                                  mz=mz_ML_cid[[x]], intensity=int_ML_cid[[x]]))
+## leaf HCD
+rt_ML_hcd <- unlist(lapply(strsplit(names(assembly_ML_hcd), split="_"), "[", 4))
+rt_ML_hcd <- as.numeric(rt_ML_hcd)
+prec_mz_ML_hcd <- unlist(lapply(strsplit(names(assembly_ML_hcd), split="_"), "[", 5))
+prec_mz_ML_hcd <- as.numeric(prec_mz_ML_hcd)
+mz_ML_hcd <- lapply(assembly_ML_hcd, function(x) x[,1])
+int_ML_hcd <- lapply(assembly_ML_hcd, function(x) x[,2])
+## create Spectrum2 objects with the corresponding information
+spN_ML_hcd_l <- lapply(1:length(assembly_ML_hcd), function(x) new("Spectrum2", 
+                                                                  rt=rt_ML_hcd[x], precursorMz=prec_mz_ML_hcd[x], 
+                                                                  mz=mz_ML_hcd[[x]], intensity=int_ML_hcd[[x]]))
+
+## sweet maize CID
+rt_swM_cid <- unlist(lapply(strsplit(names(assembly_swM_cid), split="_"), "[", 4))
+rt_swM_cid <- as.numeric(rt_swM_cid)
+prec_mz_swM_cid <- unlist(lapply(strsplit(names(assembly_swM_cid), split="_"), "[", 5))
+prec_mz_swM_cid <- as.numeric(prec_mz_swM_cid)
+mz_swM_cid <- lapply(assembly_swM_cid, function(x) x[,1])
+int_swM_cid <- lapply(assembly_swM_cid, function(x) x[,2])
+## create Spectrum2 objects with the corresponding information
+spN_swM_cid_l <- lapply(1:length(assembly_swM_cid), function(x) new("Spectrum2", 
+                                                                    rt=rt_swM_cid[x], precursorMz=prec_mz_swM_cid[x], 
+                                                                    mz=mz_swM_cid[[x]], intensity=int_swM_cid[[x]]))
+## sweet maize HCD
+rt_swM_hcd <- unlist(lapply(strsplit(names(assembly_swM_hcd), split="_"), "[", 4))
+rt_swM_hcd <- as.numeric(rt_swM_hcd)
+prec_mz_swM_hcd <- unlist(lapply(strsplit(names(assembly_swM_hcd), split="_"), "[", 5))
+prec_mz_swM_hcd <- as.numeric(prec_mz_swM_hcd)
+mz_swM_hcd <- lapply(assembly_swM_hcd, function(x) x[,1])
+int_swM_hcd <- lapply(assembly_swM_hcd, function(x) x[,2])
+## create Spectrum2 objects with the corresponding information
+spN_swM_hcd_l <- lapply(1:length(assembly_swM_hcd), function(x) new("Spectrum2", 
+                                                                    rt=rt_swM_hcd[x], precursorMz=prec_mz_swM_hcd[x], 
+                                                                    mz=mz_swM_hcd[[x]], intensity=int_swM_hcd[[x]]))
 
 ## 7)
 ## create Spectra object from the list of Spectrum2 objects
-spectra_li <- MSnbase::Spectra(spN_l, elementMetadata=S4Vectors::DataFrame(show=rep(TRUE, length(spN_l)))) 
+spectra_li_MK_cid <- MSnbase::Spectra(spN_MK_cid_l, elementMetadata=S4Vectors::DataFrame(show=rep(TRUE, length(spN_MK_cid_l)))) 
+spectra_li_MK_hcd <- MSnbase::Spectra(spN_MK_hcd_l, elementMetadata=S4Vectors::DataFrame(show=rep(TRUE, length(spN_MK_hcd_l)))) 
+spectra_li_ML_cid <- MSnbase::Spectra(spN_ML_cid_l, elementMetadata=S4Vectors::DataFrame(show=rep(TRUE, length(spN_ML_cid_l)))) 
+spectra_li_ML_hcd <- MSnbase::Spectra(spN_ML_hcd_l, elementMetadata=S4Vectors::DataFrame(show=rep(TRUE, length(spN_ML_hcd_l)))) 
+spectra_li_swM_cid <- MSnbase::Spectra(spN_swM_cid_l, elementMetadata=S4Vectors::DataFrame(show=rep(TRUE, length(spN_swM_cid_l)))) 
+spectra_li_swM_hcd <- MSnbase::Spectra(spN_swM_hcd_l, elementMetadata=S4Vectors::DataFrame(show=rep(TRUE, length(spN_swM_hcd_l)))) 
 
 ## 8) 
 ## calculate pair-wise similarities using the normalizeddotproduct
-similarityMat <- compare_Spectra(spectra_li, fun=normalizeddotproduct, binSize=0.01)
+similarityMat_MK_cid <- compare_Spectra(spectra_li_MK_cid, fun=normalizeddotproduct, binSize=0.01)
+similarityMat_MK_hcd <- compare_Spectra(spectra_li_MK_hcd, fun=normalizeddotproduct, binSize=0.01)
+similarityMat_ML_cid <- compare_Spectra(spectra_li_ML_cid, fun=normalizeddotproduct, binSize=0.01)
+similarityMat_ML_hcd <- compare_Spectra(spectra_li_ML_hcd, fun=normalizeddotproduct, binSize=0.01)
+similarityMat_swM_cid <- compare_Spectra(spectra_li_swM_cid, fun=normalizeddotproduct, binSize=0.01)
+similarityMat_swM_hcd <- compare_Spectra(spectra_li_swM_hcd, fun=normalizeddotproduct, binSize=0.01)
+
 
 ## 9) 
 ## visualize network 
 library(igraph)
+colnames(similarityMat_MK_cid) <- rownames(similarityMat_MK_cid) <- names(assembly_MK_cid)
+colnames(similarityMat_MK_hcd) <- rownames(similarityMat_MK_hcd) <- names(assembly_MK_hcd)
+colnames(similarityMat_ML_cid) <- rownames(similarityMat_ML_cid) <- names(assembly_ML_cid)
+colnames(similarityMat_ML_hcd) <- rownames(similarityMat_ML_hcd) <- names(assembly_ML_hcd)
+colnames(similarityMat_swM_cid) <- rownames(similarityMat_swM_cid) <- names(assembly_swM_cid)
+colnames(similarityMat_swM_hcd) <- rownames(similarityMat_swM_hcd) <- names(assembly_swM_hcd)
 colnames(similarityMat) <- rownames(similarityMat) <- names(assembly_MK_hcd)
-graph_from_adjacency_matrix(similarityMat, weighted = TRUE, mode = "undirected")
+similarityMat_cut <- similarityMat
+similarityMat_cut[similarityMat_cut < 0.05] <- 0
+net <- graph_from_adjacency_matrix(similarityMat_cut, weighted = TRUE, mode = "undirected", diag=F)
+plot(net, vertex.label=NA, vertex.size=0.01)
+
